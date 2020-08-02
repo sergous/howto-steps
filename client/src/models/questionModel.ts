cc9710077500d1d0809699b348cb767a26e6c608
Sergei Smirnov <sergous@gmail.com>
2020-08-02 03:16:21 -0700

    Connects question store to backend

--- client/src/models/questionModel.ts
+++ client/src/models/questionModel.ts
@@ -1,23 +1,14 @@
-import { ItemModel } from '.';
-import { QuestionStore } from '../stores';
 import { observable } from 'mobx';
 import { QuestionModelError } from '../errors';
 
-export class QuestionModel extends ItemModel {
-    @observable private query_: string;
+const Parse = require('parse');
 
-    ERROR = QuestionModelError;
+class QuestionAttributes {
+    @observable query: string = '';
+}
 
-    constructor(query: string = '', store?: QuestionStore) {
-        super(store);
-        this.query_ = query;
-    }
+export class QuestionModel extends Parse.Object {
+    @observable attributes = new QuestionAttributes();
 
-    set query(query: string) {
-        this.query_ = query;
-    }
-
-    get query() {
-        return this.query_;
-    }
+    static ERROR = QuestionModelError;
 }
--- client/src/stores/questionStore.ts
+++ client/src/stores/questionStore.ts
@@ -1,6 +1,14 @@
 import { StoreCore } from '.';
 import { QuestionModel } from '../models';
 import { QuestionStoreError } from '../errors';
+import { observable, action, computed, runInAction } from 'mobx';
+
+const Parse = require('parse');
+const { ParseMobx } = require('parse-mobx');
+
+const QUESTION = 'Question';
+
+const Question = Parse.Object.extend(QUESTION);
 
 export class QuestionStore extends StoreCore {
     ERROR = QuestionStoreError;
@@ -11,5 +19,40 @@ export class QuestionStore extends StoreCore {
 
     get questions(): QuestionModel[] {
         return <QuestionModel[]>this.items;
+    }
+
+    @action
+    async createOne(query: string): Promise<QuestionModel> {
+        const question = await new Question().set('query', query).save();
+        runInAction(() => {
+            this.questions.push(ParseMobx.toParseMobx(question));
+        });
+        return question;
+    }
+
+    @action
+    updateAttr(question: QuestionModel, name: string, value: any) {
+        question.set(name, value).save();
+    }
+
+    @action
+    async removeOne(question: QuestionModel) {
+        await question.destroy();
+        runInAction(() => {
+            ParseMobx.deleteListItem(this.questions, question);
+        });
+    }
+
+    @action
+    async fetchAll() {
+        const questions = await this.findAll();
+        runInAction(() => {
+            this.questions = ParseMobx.toParseMobx(questions);
+        });
+    }
+
+    @action
+    async findAll() {
+        return await new Parse.Query(QUESTION).find();
     }
 }
