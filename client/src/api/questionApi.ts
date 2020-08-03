d9321006a545df4a97a508242cf158de92f64bec
Sergei Smirnov <sergous@gmail.com>
2020-08-02 22:36:01 -0700

    Creates question api

---
+++ client/src/api/questionApi.ts
@@ -0,0 +1,28 @@
+import { action } from 'mobx';
+import { QuestionModel } from '../models';
+
+const Parse = require('parse');
+const { ParseMobx } = require('parse-mobx');
+
+const QUESTION = 'Question';
+
+export const Question = Parse.Object.extend(QUESTION);
+
+export class QuestionApi {
+    @action
+    async createOne(query: string) {
+        const question = await new Question().set('query', query).save();
+        return ParseMobx.toParseMobx(question);
+    }
+
+    @action
+    async deleteOne(questions: QuestionModel[], question: QuestionModel) {
+        await ParseMobx.deleteListItem(questions, question);
+    }
+
+    @action
+    async findAll() {
+        const questions = await new Parse.Query(QUESTION).find();
+        return ParseMobx.toParseMobx(questions);
+    }
+}
